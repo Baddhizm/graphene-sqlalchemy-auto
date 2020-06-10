2ee19d761db8b93cdddea54c6de5ca6dca85acbc
Baddhizm <34471467+Baddhizm@users.noreply.github.com>
2020-06-10 12:05:07 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 requirements = [
     # To keep things simple, we only support newer versions of Graphene
     "graphene-sqlalchemy>=2.3.0",
-    "graphene-sqlalchemy-filter-py35",
+    "git+git://github.com/Baddhizm/graphene-sqlalchemy-filter-py35@master#egg=graphene-sqlalchemy-filter-py35",
     "inflection"
 ]
 setuptools.setup(
@@ -40,7 +40,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.5',
-    dependency_links=[
-        "git+git://github.com/Baddhizm/graphene-sqlalchemy-filter-py35@master#egg=graphene-sqlalchemy-filter-py35"
-    ]
+    #dependency_links=[
+    #    "git+git://github.com/Baddhizm/graphene-sqlalchemy-filter-py35@master#egg=graphene-sqlalchemy-filter-py35"
+    #]
 )
