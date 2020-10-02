51297514ba2fbe14ac05221e09375d611be7eaa0
dmibes <dmitrii.beskorsyi@kronshtadt.ru>
2020-10-03 00:30:18 +0300

    Format code and sort imports

--- graphene_sqlalchemy_auto/query.py
+++ graphene_sqlalchemy_auto/query.py
@@ -7,8 +7,11 @@ import inflection
 import sqlalchemy
 from graphene import Connection, Int, Node
 from graphene.types.objecttype import ObjectType, ObjectTypeOptions
-from graphene_sqlalchemy.types import SQLAlchemyObjectType, sort_argument_for_object_type
-from graphene_sqlalchemy_filter import FilterSet, FilterableConnectionField
+from graphene_sqlalchemy.types import (
+    SQLAlchemyObjectType,
+    sort_argument_for_object_type,
+)
+from graphene_sqlalchemy_filter import FilterableConnectionField, FilterSet
 from graphene_sqlalchemy_filter.connection_field import FilterableFieldFactory
 from sqlalchemy.ext.declarative import DeclarativeMeta
 
@@ -53,16 +56,17 @@ class CustomConnection(Connection):
 
 
 def init_custom_connection_field(
-        custom_connection_field: FilterableConnectionField,
-        declarative_base: DeclarativeMeta,
-        custom_filters_path,
-        exclude_models=None,
-        base_filter_class=FilterSet
+    custom_connection_field: FilterableConnectionField,
+    declarative_base: DeclarativeMeta,
+    custom_filters_path,
+    exclude_models=None,
+    base_filter_class=FilterSet,
 ):
     if exclude_models is None:
         exclude_models = []
     models = [
-        m_cls for m_cls in declarative_base._decl_class_registry.values()
+        m_cls
+        for m_cls in declarative_base._decl_class_registry.values()
         if isinstance(m_cls, type) and issubclass(m_cls, declarative_base)
         if m_cls.__name__ not in exclude_models
     ]  # all models except exclude_models
@@ -70,22 +74,26 @@ def init_custom_connection_field(
         sqla_model: filter_factory(base_filter_class, sqla_model, custom_filters_path)()
         for sqla_model in models
     }
-    filters = {
-        **custom_connection_field.filters,
-        **generated_filters
-    }
+    filters = {**custom_connection_field.filters, **generated_filters}
     custom_connection_field.filters = filters
     custom_connection_field.factory = FilterableFieldFactory(filters)
 
 
-def filter_factory(base_filter_class: FilterSet, sqla_model: DeclarativeMeta,
-                   custom_filters_path: str = None) -> FilterSet:
-    filter_class_name = sqla_model.__name__ + 'Filter'
+def filter_factory(
+    base_filter_class: FilterSet,
+    sqla_model: DeclarativeMeta,
+    custom_filters_path: str = None,
+) -> FilterSet:
+    filter_class_name = sqla_model.__name__ + "Filter"
     try:
         # import our filters if exists
         filter_class = getattr(custom_filters_path, filter_class_name)
     except AttributeError:
-        logging.debug("Can't get {} from {} - auto generate".format(filter_class_name, custom_filters_path))
+        logging.debug(
+            "Can't get {} from {} - auto generate".format(
+                filter_class_name, custom_filters_path
+            )
+        )
         generated_fields = {
             column.key: [...] for column in sqlalchemy.inspect(sqla_model).attrs
         }
@@ -95,11 +103,11 @@ def filter_factory(base_filter_class: FilterSet, sqla_model: DeclarativeMeta,
     return filter_class
 
 
-def node_factory(custom_connection_field,
-                 model: DeclarativeMeta,
-                 custom_schemas_path: str = None) -> SQLAlchemyObjectType:
-    node_name = model.__name__ + 'Node'
-    model_description = _get_table_args_key(model, 'comment')
+def node_factory(
+    custom_connection_field, model: DeclarativeMeta, custom_schemas_path: str = None
+) -> SQLAlchemyObjectType:
+    node_name = model.__name__ + "Node"
+    model_description = _get_table_args_key(model, "comment")
 
     if hasattr(model, "id"):
         model.db_id = model.id
@@ -108,36 +116,36 @@ def node_factory(custom_connection_field,
         # import our nodes if exists
         model_node_class = getattr(custom_schemas_path, node_name)
     except AttributeError:
-        logging.debug("Can't get {} from {} - auto generate".format(node_name, custom_schemas_path))
+        logging.debug(
+            "Can't get {} from {} - auto generate".format(
+                node_name, custom_schemas_path
+            )
+        )
         meta = type(
-            'Meta',
+            "Meta",
             (object,),
             {
-                'model': model,
-                'interfaces': (Node,),
-                'connection_field_factory': custom_connection_field.factory,
-                'description': model_description
-            })
+                "model": model,
+                "interfaces": (Node,),
+                "connection_field_factory": custom_connection_field.factory,
+                "description": model_description,
+            },
+        )
         model_node_class = type(
             node_name,
             (SQLAlchemyObjectType,),
-            {
-                'db_id': Int(description='Real ID from DB'),
-                'Meta': meta
-            }
+            {"db_id": Int(description="Real ID from DB"), "Meta": meta},
         )
 
     return model_node_class
 
 
 def connections_factory(node: SQLAlchemyObjectType, custom_connection) -> Connection:
-    connection_name = node.__name__.replace('Node', 'Connection')
-    return custom_connection.create_type(
-        connection_name, node=node
-    )
+    connection_name = node.__name__.replace("Node", "Connection")
+    return custom_connection.create_type(connection_name, node=node)
 
 
-def _get_table_args_key(sqla_model: DeclarativeMeta, key: str, default=''):
+def _get_table_args_key(sqla_model: DeclarativeMeta, key: str, default=""):
     """
     Get key's value from __table_args__
     """
@@ -145,42 +153,47 @@ def _get_table_args_key(sqla_model: DeclarativeMeta, key: str, default=''):
     if isinstance(sqla_model.__table_args__, dict):
         value = sqla_model.__table_args__.get(key, default)
     elif isinstance(sqla_model.__table_args__, dict):
-        value = next((o.get(key) for o in sqla_model.__table_args__ if isinstance(o, dict)), default)
+        value = next(
+            (o.get(key) for o in sqla_model.__table_args__ if isinstance(o, dict)),
+            default,
+        )
     return value
 
 
 class QueryObjectType(ObjectType):
     @classmethod
     def __init_subclass_with_meta__(
-            cls,
-            declarative_base: DeclarativeMeta,
-            exclude_models: List[str],
-            base_filter_class=FilterSet,
-            custom_connection=CustomConnection,
-            custom_connection_field=CustomConnectionField,
-            custom_schemas_path: str = None,
-            custom_filters_path: str = None,
-            _meta=None,
-            **options
+        cls,
+        declarative_base: DeclarativeMeta,
+        exclude_models: List[str],
+        base_filter_class=FilterSet,
+        custom_connection=CustomConnection,
+        custom_connection_field=CustomConnectionField,
+        custom_schemas_path: str = None,
+        custom_filters_path: str = None,
+        _meta=None,
+        **options
     ):
-        logging.info('Generate auto query...')
+        logging.info("Generate auto query...")
         if not _meta:
             _meta = ObjectTypeOptions(cls)
         fields = OrderedDict()
         fields["node"] = graphene.relay.Node.Field()
         for model in custom_connection_field.filters:
-            logging.debug('Generate fields for {}'.format(model.__name__))
+            logging.debug("Generate fields for {}".format(model.__name__))
             node = node_factory(custom_connection_field, model, custom_schemas_path)
             connection = connections_factory(node, custom_connection)
             query_name = "%s_list" % inflection.underscore(model.__name__)
             fields.update(
                 {
-                    inflection.underscore(model.__name__): graphene.relay.Node.Field(node),
+                    inflection.underscore(model.__name__): graphene.relay.Node.Field(
+                        node
+                    ),
                     query_name: custom_connection_field(
                         connection,
                         limit=graphene.types.Int(),
                         offset=graphene.types.Int(),
-                    )
+                    ),
                 }
             )
 
@@ -188,7 +201,7 @@ class QueryObjectType(ObjectType):
             _meta.fields.update(fields)
         else:
             _meta.fields = fields
-        logging.info('Generate auto query done')
+        logging.info("Generate auto query done")
         return super(QueryObjectType, cls).__init_subclass_with_meta__(
             _meta=_meta, **options
         )
